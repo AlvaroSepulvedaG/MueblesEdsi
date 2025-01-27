86f46939d02b7ce12b74a3d032983051bed578d7
AlvaroSepulvedaG <82064367+AlvaroSepulvedaG@users.noreply.github.com>
2025-01-26 23:42:29 -0300

    seguimiento responsivo

--- src/components/ui/Seguimiento/Tracking.tsx
+++ src/components/ui/Seguimiento/Tracking.tsx
@@ -64,80 +64,82 @@ const Tracking = () => {
           }}
         ></div>
 
-        <section className="w-full max-w-4xl bg-white rounded-lg shadow-2xl overflow-hidden flex flex-col xl:flex-row">
-          <div className="xl:w-1/3 lg:w-1/2 w-full flex items-center justify-center bg-[#393839] bg-[url('/diseño-horizontal.svg')] text-white p-6">
-            <h2 className="text-2xl font-bold text-center">
-              Seguimiento de Producto
-            </h2>
-          </div>
+        <div className="container mx-auto px-4">
+          <section className="w-full max-w-4xl bg-white rounded-lg shadow-2xl overflow-hidden flex flex-col lg:flex-row">
+            <div className="lg:w-1/3 w-full flex items-center justify-center bg-[#393839] bg-[url('/diseño-horizontal.svg')] text-white p-6">
+              <h2 className="text-2xl font-bold text-center">
+                Seguimiento de Producto
+              </h2>
+            </div>
 
-          <div className="xl:w-2/3 lg:w-1/2 w-full p-8 flex flex-col">
-            <h3 className="text-center text-2xl font-semibold mb-6 text-gray-700">
-              Consulta tu Pedido
-            </h3>
-            <form
-              className="space-y-6"
-              onSubmit={(e) => {
-                e.preventDefault();
-                handleConsultar();
-              }}
-            >
-              <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-                <input
-                  type="text"
-                  placeholder="Rut"
-                  name="rut"
-                  value={rut}
-                  onChange={handleRutChange}
-                  required
-                  className="p-3 bg-white rounded-lg shadow-md outline-none border border-gray-300 focus:ring-2 focus:ring-orange-500"
-                />
-                <input
-                  type="text"
-                  placeholder="N° de pedido"
-                  name="pedido"
-                  value={pedido}
-                  onChange={(e) => setPedido(e.target.value)}
-                  required
-                  className="p-3 bg-white rounded-lg shadow-md outline-none border border-gray-300 focus:ring-2 focus:ring-orange-500"
-                />
-              </div>
-              <button
-                type="submit"
-                className="w-full py-3 bg-orange-500 text-white font-bold rounded-lg hover:bg-orange-600 focus:ring-2 focus:ring-orange-400 transition-transform transform hover:scale-105"
+            <div className="lg:w-2/3 w-full p-4 sm:p-8 flex flex-col">
+              <h3 className="text-center text-2xl font-semibold mb-6 text-gray-700">
+                Consulta tu Pedido
+              </h3>
+              <form
+                className="space-y-6"
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  handleConsultar();
+                }}
               >
-                Consultar
-              </button>
-            </form>
-            {error && (
-              <p className="text-red-500 text-center mt-4">{error}</p>
-            )}
-            {result && (
-              <div className="mt-6 p-4 bg-gray-50 rounded-lg shadow-md">
-                <p className="text-gray-700">
-                  <strong>Producto:</strong> {result.nombre_producto}
-                </p>
-                <p className="text-gray-700">
-                  <strong>Estado:</strong> {
-                    result.estado === "Listo"
-                      ? "Listo para su retiro"
-                      : result.estado
-                  }
-                </p>
-                {result.estado === "Entregado" ? (
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                  <input
+                    type="text"
+                    placeholder="Rut"
+                    name="rut"
+                    value={rut}
+                    onChange={handleRutChange}
+                    required
+                    className="min-w-[200px] p-3 bg-white rounded-lg shadow-md outline-none border border-gray-300 focus:ring-2 focus:ring-orange-500"
+                  />
+                  <input
+                    type="text"
+                    placeholder="N° de pedido"
+                    name="pedido"
+                    value={pedido}
+                    onChange={(e) => setPedido(e.target.value)}
+                    required
+                    className="min-w-[200px] p-3 bg-white rounded-lg shadow-md outline-none border border-gray-300 focus:ring-2 focus:ring-orange-500"
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="w-full py-3 bg-orange-500 text-white font-bold rounded-lg hover:bg-orange-600 focus:ring-2 focus:ring-orange-400 transition-transform transform hover:scale-105"
+                >
+                  Consultar
+                </button>
+              </form>
+              {error && (
+                <p className="text-red-500 text-center mt-4">{error}</p>
+              )}
+              {result && (
+                <div className="mt-6 p-4 bg-gray-50 rounded-lg shadow-md">
                   <p className="text-gray-700">
-                    <strong>Fecha de entrega:</strong> {result.fecha_entrega}
+                    <strong>Producto:</strong> {result.nombre_producto}
                   </p>
-                ) : result.estado !== "Listo" && (
                   <p className="text-gray-700">
-                    <strong>Fecha estimada:</strong> {result.fecha_estimada}
+                    <strong>Estado:</strong> {
+                      result.estado === "Listo"
+                        ? "Listo para su retiro"
+                        : result.estado
+                    }
                   </p>
-                )}
-                
-              </div>
-            )}
-          </div>
-        </section>
+                  {result.estado === "Entregado" ? (
+                    <p className="text-gray-700">
+                      <strong>Fecha de entrega:</strong> {result.fecha_entrega}
+                    </p>
+                  ) : result.estado !== "Listo" && (
+                    <p className="text-gray-700">
+                      <strong>Fecha estimada:</strong> {result.fecha_estimada}
+                    </p>
+                  )}
+                  
+                </div>
+              )}
+            </div>
+          </section>
+        </div>
       </main>
 
       <footer className="bg-gray-200 text-center py-4">
